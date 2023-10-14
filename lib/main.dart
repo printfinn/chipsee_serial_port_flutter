8a60c446a8307266d7be4d7357f631e335aa9f45
printfinn <git@printf.win>
2023-10-14 21:40:54 +0800

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MySerialApp());
+}
+
+class MySerialApp extends StatelessWidget {
+  const MySerialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Placeholder(),
+    );
+  }
+}
