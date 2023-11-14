c516a176e112b5b3fdec5104e154979dfa614d46
printfinn <git@printf.win>
2023-11-14 10:59:41 +0800

    Add a bottom navigation bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:serial_app/serial_receiver_screen.dart';
 import 'package:serial_app/serial_screen.dart';
 
 void main() {
@@ -10,8 +11,49 @@ class MySerialApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SerialScreen(),
+    return const MaterialApp(
+      home: BottomNavi(),
+    );
+  }
+}
+
+class BottomNavi extends StatefulWidget {
+  const BottomNavi({super.key});
+
+  @override
+  State<BottomNavi> createState() => _BottomNaviState();
+}
+
+class _BottomNaviState extends State<BottomNavi> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber[800],
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Send',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.business),
+            label: 'Receive',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        const SerialScreen(),
+        const SerialReceiverScreen(),
+      ][currentPageIndex],
     );
   }
 }
